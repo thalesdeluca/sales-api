acadeef683fe6e6df6db39415001d4955504625a
Thales de Luca <delucathales@gmail.com>
2019-03-19 18:20:36 -0300

    Added HSQLDB Database to dependencies, and connected with DAO layer

--- src/main/java/com/thalesdeluca/salesapi/Controller/SellerController.java
+++ src/main/java/com/thalesdeluca/salesapi/Controller/SellerController.java
@@ -4,14 +4,11 @@ import com.thalesdeluca.salesapi.Entity.Seller;
 import com.thalesdeluca.salesapi.Service.SellerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
-@Controller
+@RestController
 @RequestMapping("/sellers")
 public class SellerController {
 
--- src/main/java/com/thalesdeluca/salesapi/Dao/ISaleDao.java
+++ src/main/java/com/thalesdeluca/salesapi/Dao/ISaleDao.java
@@ -2,5 +2,7 @@ package com.thalesdeluca.salesapi.Dao;
 
 import com.thalesdeluca.salesapi.Entity.Sale;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ISaleDao extends CrudRepository<Sale, Integer> {}
