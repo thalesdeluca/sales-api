454fa57d9c8617667e2cd15dfcb1aadf033b3f02
Thales de Luca <delucathales@gmail.com>
2019-03-19 13:56:44 -0300

    Generated Project in Spring Initializr

---
+++ src/main/java/com/thalesdeluca/salesapi/SalesApiApplication.java
@@ -0,0 +1,13 @@
+package com.thalesdeluca.salesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesApiApplication.class, args);
+	}
+
+}
